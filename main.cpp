43a3893bbf672852214d016d350ee6484153d105
Hamza28Salhi <hamza.salhi@esprit.tn>
2021-11-01 05:13:51 -0700

    Maquettes Design du projet

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
-#include <ios stream>
-#include <string>
-using namespace std;
-int main ()
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connection.h"
+
+int main(int argc, char *argv[])
 {
-    cout<<"Hello world!!"<<endl;
-    cout<<"Hello world!!"<<endl;
-    return 0 ;
+    QApplication a(argc, argv);
+    MainWindow w;
+    connection c;
+    bool test=c.createconnection();
+    if(test)
+    {w.show();
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
+                    QObject::tr("connection successful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+    return a.exec();
 }
